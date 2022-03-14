300d441efde6829f1d2b4d67c87cec23d7ffe758
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-03-13 23:20:43 -0400

    navbar elements added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from './components/About';
+import Nav from './components/Nav';
+import './App.css';
+
+function App() {
+    return ( <div >
+            <Nav></Nav>
+        <main >
+        <About> </About> 
+        </main> 
+        </div>
+    );
+}
+
+export default App;
